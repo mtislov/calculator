092a22900b4dd3302be4830f015066a581844d50
Roi <vosroix@gmail.com>
2020-07-06 14:52:02 +0500

    added new functions for the calculator

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('ji');
+
+
+
+
+
+const calcBtnArr = Array.from(document.querySelectorAll('.calculator__btn'));
+const calcSreen = document.querySelector('.calculator__screen');
+
+function addValue(value) {
+    calcSreen.value += value;
+}
+
+function calcClear() {
+    calcSreen.value = '';
+}
