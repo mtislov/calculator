9c5ef388c25b71d6937187ae98b1e2bfa327368b
Roi <vosroix@gmail.com>
2020-07-06 17:10:29 +0500

    added a new calcValidate func

--- app.js
+++ app.js
@@ -5,6 +5,25 @@
 
 const calcBtnArr = Array.from(document.querySelectorAll('.calculator__btn'));
 const calcSreen = document.querySelector('.calculator__screen');
+const calcOperators = ['/', '*', '-', '+', '.'];
+
+
+function calcValidate (operator) {
+    let val = calcSreen.value;
+
+    if (val == '' && operator !== '-') return false;
+
+    calcOperators.forEach(op => {
+        if (op == val.slice(-1)) calcClearLast();
+
+    });
+
+    return true;
+}
+
+function addOperator(operator) {
+    if ( calcValidate(operator) ) addValue(operator);
+}
 
 function addValue(value) {
     calcSreen.value += value;
@@ -12,4 +31,15 @@ function addValue(value) {
 
 function calcClear() {
     calcSreen.value = '';
+}
+
+function calcClearLast() {
+    calcSreen.value = calcSreen.value.slice(0, -1)
+}
+
+function calculate() { 
+    if (calcSreen.value == '') return false;
+
+    calcSreen.value = eval(calcSreen.value);
+    console.log(eval(calcSreen.value));
 }
